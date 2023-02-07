3a25029a5498191470c4d6c6c68a1fe3c269a839
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2023-02-07 15:44:59 +0500

    completed

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const connectDB = require('./db/connect');
+const tasks = require('./routes/tasks');
+
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+// routes
+
+app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
